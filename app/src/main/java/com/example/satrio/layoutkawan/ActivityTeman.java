26e31fe5c2269c9fdb8167efa92961e34930426b
Satrio <satrioarbi07@gmail>
2017-05-07 11:51:51 +0700

    ini tuh layout barengan

---
+++ app/src/main/java/com/example/satrio/layoutkawan/ActivityTeman.java
@@ -0,0 +1,13 @@
+package com.example.satrio.layoutkawan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityTeman extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teman);
+    }
+}
